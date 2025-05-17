091d42ae3c986f336c577bf9a507bacda185673a
Azegrarilham <ilhamzgrr@gmail.com>
2025-05-17 12:20:06 +0100

    comments

--- run-scheduler.ps1
+++ run-scheduler.ps1
@@ -1,8 +1,8 @@
 while ($true) {
     Write-Host "Running cancel-abandoned command..."
     php artisan reservations:cancel-abandoned
-    Write-Host "Waiting 2 minutes..."
-    Start-Sleep -Seconds 120
+    Write-Host "Waiting 3 minutes..."
+    Start-Sleep -Seconds 180
 }
 // This script runs the `php artisan reservations:cancel-abandoned` command every 60 seconds.
 // 5min = 300 seconds 3 = 180
